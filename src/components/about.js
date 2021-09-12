f298f3522935a7439826c83ecd38629f16ded7e2
Camden McKenzie <camden.mckenzie@outlook.com>
2021-09-12 19:08:19 -0400

    Initial Commit

---
+++ src/components/about.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../yourdata"
+
+const About = () => {
+  return (
+    <div className="secion" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content">
+            <Fade bottom cascade>
+              <h1>About Us</h1>
+              <h2>Kyle and Katie Black</h2>
+            </Fade>
+            <p>
+              {data.aboutParaOne}
+              <br></br>
+              <br></br>
+              {data.aboutParaTwo}
+              <br></br>
+              <br></br>
+              {data.aboutParaThree}
+              <br></br>
+              <br></br>
+              {data.aboutParaFour}
+            </p>
+          </div>
+          <div className="image-wrapper">
+            <img src={data.aboutImage} alt="about"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/yourdata.js
@@ -0,0 +1,205 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+  //   Navbar
+  company_icon: "https://static.wixstatic.com/media/5f6462_64568628f2994ccca1763e5ee18a1972~mv2.png/v1/fill/w_332,h_332,al_c,q_85,usm_0.66_1.00_0.01/ADA4888D-2B4A-42CD-8EBC-2C68D6CD6F2B_edi.webp",
+
+  //   Header Details ---------------------
+  company_name1: "Our Family",
+  company_name2: "Handyman",
+  headerTagline: [
+    //Line 1 For Header
+    "Trustworthy",
+    //Line 2 For Header
+    "Local",
+    //Line 3 For Header
+    "Reliable",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "We are a professional company, meaning we are drug free, dependable and have great rates.",
+
+  //Contact Email
+  contactEmail: "camden.mknz@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Master Bedroom Remodel", //Project Title - Add Your Project Title Here
+      para:
+        "Updated this master bedroom with a new oak hardwood floor.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_d3c9224289f84d68911053e4aa449d68~mv2.jpg/v1/fill/w_902,h_676,al_c,q_85,usm_0.66_1.00_0.01/5C5DD775-A704-42BA-B890-133FD8C09508_JPG.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Bathroom Remodel", //Project Title - Add Your Project Title Here
+      para:
+        "Updated master suite bathroom.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_99d1578c2dc6494b81d28d800ca76dbd~mv2.png/v1/fill/w_716,h_538,al_c,q_90,usm_0.66_1.00_0.01/F301DBEF-3A71-4ECF-87AF-2700BDF5BD9B_heic.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "House Painting Exterior", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_3fc73be7bc354719b8498d161d617406~mv2.jpeg/v1/fill/w_930,h_698,al_c,q_85,usm_0.66_1.00_0.01/819523AC-70A5-4DD5-B79B-577B9AD09847.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Kitchen Remodel", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_8b872349dba74547a561bd472ed607c1~mv2.jpeg/v1/fill/w_524,h_698,al_c,q_85,usm_0.66_1.00_0.01/4B8EFA1B-5A0E-488C-999D-32AD2008667D.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "House Painting Exterior", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_3fc73be7bc354719b8498d161d617406~mv2.jpeg/v1/fill/w_930,h_698,al_c,q_85,usm_0.66_1.00_0.01/819523AC-70A5-4DD5-B79B-577B9AD09847.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Master Bedroom Remodel", //Project Title - Add Your Project Title Here
+      para:
+        "Updated this master bedroom with a new oak hardwood floor.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://static.wixstatic.com/media/5f6462_d3c9224289f84d68911053e4aa449d68~mv2.jpg/v1/fill/w_902,h_676,al_c,q_85,usm_0.66_1.00_0.01/5C5DD775-A704-42BA-B890-133FD8C09508_JPG.webp",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I have been in the construction industry for over 20 years. With a short break after the 08’ market crash. When I was 15, I started working as a laborer for a landscaping company in Portland, since this was seasonal, I had to find work for the winters and started working for various construction companies. The companies I worked for specialized in different areas of construction, two built new homes, one focused on roofing, and other home remodels, bathrooms and kitchens.",
+  aboutParaTwo:
+    "I have owned and operated a couple other businesses, one was a landscaping business and the other was a construction company. These businesses were great. I closed the landscaping business down when the market crashed in 08’. We moved to Central Maine, and I worked for a while for a government agency. In 15’ we started the construction company that we had till 18’ when I got sick. I was told that I could not do any physically strenuous work for 18 months and spent 4 months recovering from a series of surgeries and hospitalizations. In July of 19’ I received a clean bill of health and was told I could get back at it.",
+  aboutParaThree:
+    "So long story short, my wife, Katie, and I sat down and looked at what type of business we wanted to have. After some research and bouncing ideas off each other we launched, Handyman Services of Maine, yet we did not like the name and for over a year worked on a new name for the company. In January to 21’ we change our name to Our Family Handyman, which is a better reflection of the company we wanted. A name that we would be proud of, and our children could join if they choose to in the future. Our logo is the silhouette of our 5 children. ",
+  aboutParaFour:
+    "We started this company to reflect our values, to provide exceptional services to all our customers and build a great name that people depend on. We look forward to working for you.",
+  aboutImage:
+    "https://static.wixstatic.com/media/5f6462_e075518a0d4948f89d23006b5641d56a~mv2_d_1247_2471_s_2.jpg/v1/crop/x_0,y_268,w_1247,h_1897/fill/w_586,h_892,al_c,q_85,usm_0.66_1.00_0.01/Katie%2009_edited_edited.webp",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "Have a Home Repair problem that needs some professional attention? No matter what your vision is, Our Family Handyman can be trusted to help you bring it to life. If you need repairs done, need assistance with installation, or would like a remodeling upgrade, give us a call and we will offer you the services you need at prices you can afford.",
+    },
+    {
+      img: cssIcon,
+      para:
+        "We have years of experience with Cabinet Installation, Kitchen Remodel and bathroom remodel and guarantee the quality of our workmanship. Whatever your project, we are ready to help. Call us now to schedule an appointment and free estimate. We will also answer any questions you may have about how we work and what to expect during your service appointment.",
+    },
+    {
+      img: jsIcon,
+      para:
+        "Need a handyman to tackle your deck project? You name it, we can do it! We supply both the materials and labor for any type of deck job. We perform the repairs and improvements you need with the experience, skill, and customer service you deserve. Feel free to call for more information or to set up an appointment.",
+    },
+    {
+      img: reactIcon,
+      para:
+        "Looking to update your bathroom or kitchen with some fresh paint or a complete makeover. Call us to help you make your dream kitchen or bathroom a reality.",
+    },
+    {
+      img: designIcon,
+      para:
+        "Want to sell your home and do not have the time to do it. Give us a call. We can do the little touch ups and modifications that you and your Realtor discuss.",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Do you need someone to come and take care of the task you don't have time to, give us a call and I will make your honey do list a list of the past.",
+    },
+    // {
+    //   img: codeIcon,
+    //   para:
+    //     "Do you have a home or camp in Maine that you would like to have checked on or maintained while you are away. Give us a call and we will help give you peace of mind knowing your property is taken care of.",
+    // },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Vivid Vision",
+  promotionPara:
+    "It has our core values, our goals and the desire we have as a company. As a company we do a weekly review of a section of our Vivid Vision this reminds us where we are going and where we can improve to accomplish our vision as a company. It is also a document we use in guiding us in hiring team members.  ",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/chetanverma",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
